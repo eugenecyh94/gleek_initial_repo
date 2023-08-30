684d22a2647917041e33aabe61ac38a4836d7fd3
DESKTOP-PR2QQ8F\CYH_Eugene <eugenecyh94@gmail.com>
2023-08-30 14:04:58 +0800

    Ran Prettier

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -4,13 +4,12 @@ import ActivityList from "./components/activity/ActivityList";
 import SocketConnection from "./utils/SocketConnection";
 
 function App() {
-
   return (
     <div>
-        <SocketConnection/>
-        <Routes>
-            <Route exact path="/" element={<ActivityList />} />
-        </Routes>
+      <SocketConnection />
+      <Routes>
+        <Route exact path="/" element={<ActivityList />} />
+      </Routes>
     </div>
   );
 }
--- admin-frontend/src/components/activity/ActivityList.jsx
+++ admin-frontend/src/components/activity/ActivityList.jsx
@@ -2,7 +2,7 @@ import { Grid, styled } from "@mui/material";
 import ActivityListItem from "./ActivityListItem";
 import { useEffect, useState } from "react";
 import { updateAllActivity, useActivityStore } from "../../zustand/GlobalStore";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 
 const GridStyle = styled(Grid)(({ theme }) => ({
   marginTop: theme.spacing(3),
@@ -16,29 +16,33 @@ const ActivityList = () => {
 
   useEffect(() => {
     useActivityStore.subscribe((newState, prevState) => {
-      console.log("activity store detected newState::", newState.allActivities.data);
-      updateAllActivities(newState.allActivities.data)
-    })
+      console.log(
+        "activity store detected newState::",
+        newState.allActivities.data,
+      );
+      updateAllActivities(newState.allActivities.data);
+    });
     updateIsLoading(false);
-  }, [isLoading, updateIsLoading, updateAllActivities])
+  }, [isLoading, updateIsLoading, updateAllActivities]);
 
   useEffect(() => {
     console.log("isloading status::", isLoading);
-    console.log("allActivityState::", allActivities)
-  },[isLoading, allActivities])
-
+    console.log("allActivityState::", allActivities);
+  }, [isLoading, allActivities]);
 
   return (
-      <>
-        {isLoading && <CircularProgress/>}
-        {!isLoading && <GridStyle container spacing={3}>
+    <>
+      {isLoading && <CircularProgress />}
+      {!isLoading && (
+        <GridStyle container spacing={3}>
           {allActivities.map((activity) => (
-              <Grid key={activity.name} item xs={12} sm={6} md={4} lg={3}>
-                <ActivityListItem key={activity.name} activity={activity}/>
-              </Grid>
+            <Grid key={activity.name} item xs={12} sm={6} md={4} lg={3}>
+              <ActivityListItem key={activity.name} activity={activity} />
+            </Grid>
           ))}
-        </GridStyle>}
-      </>
+        </GridStyle>
+      )}
+    </>
   );
 };
 
