a3e3033aae1706df96c9403cccc996432de82bd6
DESKTOP-PR2QQ8F\CYH_Eugene <eugenecyh94@gmail.com>
2023-08-30 14:04:58 +0800

    Ran Prettier

--- admin-frontend/src/utils/SocketConnection.jsx
+++ admin-frontend/src/utils/SocketConnection.jsx
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
-import {
-  updateAllActivity,
-} from "../zustand/GlobalStore";
+import { updateAllActivity } from "../zustand/GlobalStore";
 import AxiosConnect from "./AxiosConnect";
 
 const SocketConnection = () => {
-
   const subscribeActivitiesData = () => {
     AxiosConnect.get("activity/all")
       .then((body) => {
