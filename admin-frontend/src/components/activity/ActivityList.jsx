68bf34969bf1d82fcb79b5549ec94ce6fc421d74
DESKTOP-PR2QQ8F\CYH_Eugene <eugenecyh94@gmail.com>
2023-08-30 13:51:47 +0800

    Updated admin-frontend 'ActivityList.jsx' to allow data retrieved from backend to be loaded instead of mock data.

--- admin-frontend/src/components/activity/ActivityList.jsx
+++ admin-frontend/src/components/activity/ActivityList.jsx
@@ -2,6 +2,7 @@ import { Grid, styled } from "@mui/material";
 import ActivityListItem from "./ActivityListItem";
 import { useEffect, useState } from "react";
 import { updateAllActivity, useActivityStore } from "../../zustand/GlobalStore";
+import CircularProgress from '@mui/material/CircularProgress';
 
 const GridStyle = styled(Grid)(({ theme }) => ({
   marginTop: theme.spacing(3),
@@ -11,43 +12,33 @@ const ActivityList = () => {
   const [allActivities, updateAllActivities] = useState(
     useActivityStore.getState().allActivities,
   );
+  const [isLoading, updateIsLoading] = useState(true);
 
-  const MockData = [
-    {
-      _id: "64eddea82965fac7c8a47860",
-      title: "Airport Event",
-      description: "This is an airport event",
-      price: 20,
-      image:
-        "https://www.visitsingapore.com/content/dam/mice2-0/plan-your-event/sustainability/host-a-sustainable-event-1-360x203.jpg",
-    },
-    {
-      _id: "64eddfad2965fac7c8a47861",
-      title: "MBS Event",
-      description: "This is an mbs event",
-      price: 15,
-      image:
-        "https://www.visitsingapore.com/content/dam/mice2-0/plan-your-event/sustainability/host-a-sustainable-event-2-360x203.jpg",
-    },
-    {
-      _id: "64ede00f2965fac7c8a47862",
-      title: "Farm Event",
-      description: "This is a farm event",
-      price: 10,
-      image:
-        "https://www.visitsingapore.com/content/dam/mice2-0/plan-your-event/sustainability/host-a-sustainable-event-3-360x203.jpg",
-    },
-  ];
+  useEffect(() => {
+    useActivityStore.subscribe((newState, prevState) => {
+      console.log("activity store detected newState::", newState.allActivities.data);
+      updateAllActivities(newState.allActivities.data)
+    })
+    updateIsLoading(false);
+  }, [isLoading, updateIsLoading, updateAllActivities])
+
+  useEffect(() => {
+    console.log("isloading status::", isLoading);
+    console.log("allActivityState::", allActivities)
+  },[isLoading, allActivities])
+
 
   return (
-    //{allActivities.data.map((activity) => (
-    <GridStyle container spacing={3}>
-      {MockData.map((activity) => (
-        <Grid key={activity.name} item xs={12} sm={6} md={4} lg={3}>
-          <ActivityListItem key={activity.name} activity={activity} />
-        </Grid>
-      ))}
-    </GridStyle>
+      <>
+        {isLoading && <CircularProgress/>}
+        {!isLoading && <GridStyle container spacing={3}>
+          {allActivities.map((activity) => (
+              <Grid key={activity.name} item xs={12} sm={6} md={4} lg={3}>
+                <ActivityListItem key={activity.name} activity={activity}/>
+              </Grid>
+          ))}
+        </GridStyle>}
+      </>
   );
 };
 
