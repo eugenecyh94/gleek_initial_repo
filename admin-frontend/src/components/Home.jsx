1ccdd500829b1b1724ec27d769316a3c286f449a
Pui Yee Tong <e0560031@u.nus.edu>
2023-09-02 01:26:11 +0800

    create navbar outline UI

---
+++ admin-frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import HomePageNavBar from "./navbar/HomePageNavBar";
+import SideNavBar from "./navbar/SideNavBar";
+
+const Home = () => {
+  return (
+    <>
+      <HomePageNavBar />
+      <SideNavBar />
+    </>
+  );
+};
+export default Home;
