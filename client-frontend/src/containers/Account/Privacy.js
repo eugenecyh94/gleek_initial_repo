bc67009ab22828656b82a5bc5bda913d36bf986d
angler <angler.ghy@gmail.com>
2023-09-02 00:30:40 +0800

    Add open T&C button

--- client-frontend/src/containers/Account/Privacy.js
+++ client-frontend/src/containers/Account/Privacy.js
@@ -1,34 +1,27 @@
 import React from "react";
 import {
   Box,
-  TextField,
   Button,
   Typography,
-  InputAdornment,
-  IconButton,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  FormHelperText,
-  Grid,
-  MenuItem,
-  Menu,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
+  Modal,
   FormGroup,
   FormControlLabel,
   Checkbox,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 import AccountSidebar from "./AccountSidebar";
 
 function Privacy(props) {
   const theme = useTheme();
   const tertiary = theme.palette.tertiary.main;
   const primary = theme.palette.primary.main;
+
+  // Modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Box
       display="flex"
@@ -69,6 +62,41 @@ function Privacy(props) {
             label="I agree to the Terms & Conditions of Gleek."
             disabled={true}
           />
+          <Button width="5rem" onClick={handleOpen}>
+            Open T&C
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "80%",
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                borderRadius: "25px",
+                p: 4,
+              }}
+            >
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Terms & Conditions
+              </Typography>
+              <Typography
+                id="modal-modal-description"
+                overflow="auto"
+                maxHeight="30em"
+                sx={{ mt: 2 }}
+              >
+                {new Array(2000).fill("text").join(" ")}
+              </Typography>
+            </Box>
+          </Modal>
         </FormGroup>
 
         <Button
