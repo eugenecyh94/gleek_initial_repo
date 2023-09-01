1093342d710046fc6506a49f6c53c7c99ce41c59
angler <angler.ghy@gmail.com>
2023-09-01 21:42:44 +0800

    Fix account path

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -6,8 +6,8 @@ import HomePage from "./containers/HomePage";
 import Layout from "./components/Layout";
 import LoginPage from "./containers/LoginPage";
 import RegisterPage from "./containers/RegisterPage";
-import AccountDetails from "./containers/Settings/AccountDetails";
-import Privacy from "./containers/Settings/Privacy";
+import AccountDetails from "./containers/Account/AccountDetails";
+import Privacy from "./containers/Account/Privacy";
 
 function App() {
   return (
