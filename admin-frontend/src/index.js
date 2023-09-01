06672e3d0b2cd492adc51c46f5af0ee01d1eca11
Pui Yee Tong <e0560031@u.nus.edu>
2023-09-02 01:26:11 +0800

    Rebased Yunus branch onto weekly merged branch

--- admin-frontend/src/index.js
+++ admin-frontend/src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import theme from "./themes/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
   // </React.StrictMode>
 );
 
