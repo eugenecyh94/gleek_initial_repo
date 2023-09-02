40853d21040c899dcdc6d4d06d244c221a2fc4f1
Yunus <yunusali150201@gmail.com>
2023-09-02 16:10:36 +0800

    Added authentication and dummy user model

---
+++ server/model/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+});
+const User = mongoose.model("User", userSchema, "users");
+
+export default User;
