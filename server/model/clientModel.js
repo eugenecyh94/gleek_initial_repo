e8449106751821379753dca48f3c2001d1e65e88
angler <angler.ghy@gmail.com>
2023-09-12 14:29:28 +0800

    Update client model (#17)
    * Update clientModel
    
    * Change phoneNumber to String

--- server/model/clientModel.js
+++ server/model/clientModel.js
@@ -11,6 +11,27 @@ const clientSchema = mongoose.Schema({
     required: true,
     minlength: 8,
   },
+  name: { type: String, required: false },
+  phoneNumber: { type: String, required: false },
+  jobTitle: { type: String, required: false },
+  team: { type: String, required: false },
+  companyName: { type: String, required: false },
+  officeAddress: { type: String, required: false },
+  officePostalCode: { type: Number, required: false },
+  billingPartyName: { type: String, required: false },
+  billingAddress: { type: String, required: false },
+  billingOfficePostalCode: { type: Number, required: false },
+  billingEmail: { type: String, required: false },
+  signupDate: { type: Date, required: false },
+  approvedDate: { type: Date, required: false },
+  emergencyContactName: { type: String, required: false },
+  emergencyContactNumber: { type: String, required: false },
+  photo: { type: String, required: false },
+  status: {
+    type: String,
+    enum: ["PENDING", "APPROVED", "REJECTED"],
+    required: false,
+  },
 });
 
 const ClientModel = mongoose.model("Client", clientSchema, "clients");
