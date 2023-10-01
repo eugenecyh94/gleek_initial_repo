1e843488102556e4b7b09fc8015e239dbc9919d8
yiying <yowyiying@gmail.com>
2023-10-01 11:42:13 +0800

    Removed ResetPassword component - not in use anymore

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -6,7 +6,6 @@ import LoginPage from "./components/LoginPage";
 import ViewPublishedActivities from "./components/activity/ViewPublishedActivities";
 import ProtectedRoute from "./components/ProtectedRoutes.jsx";
 import ChangePassword from "./components/profile/ChangePassword.jsx";
-import ResetPassword from "./components/ResetPassword";
 import ViewAllVendors from "./components/vendor/ViewAllVendors";
 import ViewAllClients from "./components/client/ViewAllClients";
 import ClientDetails from "./components/client/ClientDetails";
@@ -82,15 +81,6 @@ function App() {
               </ProtectedRoute>
             }
           />
-          {/* <Route
-            exact
-            path="/resetPassword"
-            element={
-              <ProtectedRoute>
-                <ResetPassword />
-              </ProtectedRoute>
-            }
-          /> */}
           <Route
             exact
             path="/manageProfile/changePassword"
