9786f294cc0c86e1a512a4306644429c31000301
Chua You Heng Eugene <80498187+eugenecyh94@users.noreply.github.com>
2023-10-16 15:01:26 +0800

    Added delete and mark as read function to admin notification. (#122)

--- admin-frontend/src/utils/SocketConnection.js
+++ admin-frontend/src/utils/SocketConnection.js
@@ -4,24 +4,14 @@ import AxiosConnect from "./AxiosConnect.js";
 
 const SocketConnection = () => {
   const { setAuthenticated, setAdmin, admin } = useAdminStore();
-  const { setReceivedNotifications } = useNotificationStore();
+  const { retrieveAndSetAllNotifications } = useNotificationStore();
   const initialiseData = async () => {
     try {
       const response = await AxiosConnect.post("/gleekAdmin/validate-token");
       const data = response.data;
       setAuthenticated(true);
       setAdmin(data.admin);
-      const params = {
-        adminId: data.admin._id,
-        adminRole: data.admin.role,
-      };
-      const notificationResponse = await AxiosConnect.getWithParams(
-        "/notification/adminAllNotifications",
-        params,
-      ).then((body) => {
-        console.log(body.data.data);
-        setReceivedNotifications(body.data.data);
-      });
+      retrieveAndSetAllNotifications(data.admin);
     } catch (error) {
       setAuthenticated(false);
       setAdmin(null);
--- server/routes/notificationRoute.js
+++ server/routes/notificationRoute.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   getAdminNotifications,
   getNonAdminNotifications,
+  updateNotificationAsRead,
+  updateAllNotificationsAsRead,
+  deleteNotification,
 } from "../controller/notificationController.js";
 import { verifyToken } from "./../middleware/clientAuth.js";
 
@@ -14,5 +17,11 @@ const router = express.Router();
 router.get("/adminAllNotifications", getAdminNotifications);
 
 router.get("/nonAdminAllNotifications", verifyToken, getNonAdminNotifications);
+
+router.patch("/updateNotificationAsRead", updateNotificationAsRead);
+
+router.patch("/updateAllNotificationsAsRead", updateAllNotificationsAsRead);
+
+router.patch("/deleteNotification", deleteNotification);
 
 export default router;
